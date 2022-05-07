246738059c170ec0b6460c844e0f0b82e1ff8600
Andreas Palmgren <andreas.l.palmgren@gmail.com>
2022-05-07 17:26:28 +0200

    plot

--- stock.py
+++ stock.py
@@ -15,6 +15,8 @@ def set_style():
         "axes.spines.right" : False,
         "axes.spines.bottom" : True,
         "axes.spines.top" : False,
+        "axes.edgecolor" : "#512f5c",
+        "axes.linewidth" : 6, 
         "xtick.bottom" : True,
         "xtick.labelbottom" : True,
         "ytick.labelleft" : True,
@@ -28,19 +30,29 @@ def set_style():
 
 def update_gspc(start_value=10000, plot_gspc=True):
 
-    df_gspc = yf.download("^GSPC", "2022-03-14", "2022-04-15")["Close"]
+    df_gspc = yf.download("^GSPC", "2021-03-14", "2022-04-15")["Close"]
+    df_gspc = (1+df_gspc.pct_change()).cumprod() * start_value
+    df_gspc[0] = start_value
 
+    # Save portfolio value
     filepath = Path('data/gspc_data.csv')
-    filepath.parent.mkdir(parents=True, exist_ok=True)  
+    filepath.parent.mkdir(parents=True, exist_ok=True)
 
     df_gspc.to_csv(filepath)
 
+    # Plot
     if plot_gspc: 
         set_style()
 
-        fig, axs = plt.subplots(1, figsize=(15, 12))
+        fig, axs = plt.subplots(1, figsize=(8, 8))
+
+        fig.patch.set_facecolor('#13011a')
+        axs.set_facecolor('#13011a')
+        axs.tick_params(axis='x', colors='#b760d1')
+        axs.tick_params(axis='y', colors='#b760d1')
+
         axs.plot(df_gspc)
-        plt.savefig("image\gspc.png")
+        plt.savefig("docs\images\gspc.png")
 
 if __name__ == "__main__":
     update_gspc()
